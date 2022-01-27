845876fe96c42c904e1965b26b3337392baa7846
RachelSparto <rmbachman2016@hotmail.com>
2022-01-26 19:55:33 -0500

    TimerControl is working

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -132,44 +132,12 @@ function Pomodoro() {
         focusDuration={focusDuration}
         breakDuration={breakDuration}
       />
-      <TimerControl />
-      <div className="row">
-        <div className="col">
-          <div
-            className="btn-group btn-group-lg mb-2"
-            role="group"
-            aria-label="Timer controls"
-          >
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-testid="play-pause"
-              title="Start or pause timer"
-              onClick={playPause}
-            >
-              <span
-                className={classNames({
-                  oi: true,
-                  "oi-media-play": !isTimerRunning,
-                  "oi-media-pause": isTimerRunning,
-                })}
-              />
-            </button>
-            {/* Implement stopping the current focus or break session. and disable the stop button when there is no active session */}
-            {/* Disable the stop button when there is no active session */}
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-testid="stop"
-              title="Stop the session"
-              onClick={handleStopSession}
-              disabled={!session}
-            >
-              <span className="oi oi-media-stop" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <TimerControl
+        isTimerRunning={isTimerRunning}
+        handleStopSession={handleStopSession}
+        playPause={playPause}
+        session={session}
+      />
 
       <SessionControl />
       <div>
